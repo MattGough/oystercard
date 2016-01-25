0db48e257b764d383314e64ab76f341634b95904
Matt <mattpgough@gmail.com>
2016-01-25 19:42:56 +0000

    Added unit tests for Oystercard class:'has a balance','top-up','can add amount to balance.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,13 @@
-describe Oystercard do
-	
+require 'oystercard'
+
+  describe Oystercard do
+	it 'has a balance' do
+	expect(subject.balance).to eq(0)
+  end
+  describe '#top_up' do
+   it {is_expected.to respond_to(:top_up).with(1).argument}
+   end
+   it 'can add amount to balance' do
+   	expect{subject.top_up 1}.to change{subject.balance}.by 1
+   end
 end
